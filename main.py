e033e9af939308b8377223d31240d292398ef366
PYLjs <69771116+PYLjs@users.noreply.github.com>
2021-12-15 18:19:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ st.markdown(html_temp, unsafe_allow_html=True)
 st.image("Capture.JPG")
 st.subheader('by Paul-Yuthi Lajus ')
 
-### load file
 uploaded_file = st.file_uploader("Choose an image file", type="jpg")
 
 map_dict = {0: 'snowing',
@@ -28,12 +27,12 @@ map_dict = {0: 'snowing',
 
 
 if uploaded_file is not None:
-    # Convert the file to an opencv image.
+    
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
     resized = cv2.resize(opencv_image,(224,224))
-    # Now do something with the image! For example, let's display it:
+    
     st.image(opencv_image, channels="RGB")
 
     resized = mobilenet_v2_preprocess_input(resized)
